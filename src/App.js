b06eecfaa3057b7de8537ad36babcf11e5e2cf96
leandrotalenton <leandrotalenton@hotmail.com>
2022-04-07 16:15:58 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>Hola Mundo!</div>
+/*     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div> */
   );
 }
 
