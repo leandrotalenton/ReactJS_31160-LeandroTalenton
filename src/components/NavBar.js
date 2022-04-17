1565b4d7c7240e79862bf3e41f28ceef43b7f0e6
Leandro T <leandrotalenton@hotmail.com>
2022-04-17 20:40:44 -0300

    cambio de color

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,18 @@
 const NavBar = () => {
 	return (
-		<div className="navbar bg-base-100">
+		<div className="navbar bg-base-300 bg-neutral">
 			<div className="flex-1">
 				<a href="./#" className="btn btn-ghost normal-case text-xl" >Tiendita</a>
 			</div>
 			<div className="flex-none">
 				<ul className="menu menu-horizontal p-0">
 					<li><a href="./#">Item 1</a></li>
-					<li tabindex="0">
+					<li tabIndex="0">
 						<a href="./#">
 							Parent
 							<svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
 						</a>
-						<ul className="p-2 bg-base-100">
+						<ul className="p-2 bg-base-300">
 							<li><a href="./#">Submenu 1</a></li>
 							<li><a href="./#">Submenu 2</a></li>
 						</ul>
@@ -28,7 +28,7 @@ const NavBar = () => {
 							<span className="badge badge-sm indicator-item">8</span>
 						</div>
 					</label>
-					<div tabIndex="0" className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow">
+					<div tabIndex="0" className="mt-3 card card-compact dropdown-content w-52 bg-base-300 shadow">
 						<div className="card-body">
 							<span className="font-bold text-lg">8 Items</span>
 							<span className="text-info">Subtotal: $999</span>
@@ -44,7 +44,7 @@ const NavBar = () => {
 							<img src="https://api.lorem.space/image/face?hash=33791" alt="img"/>
 						</div>
 					</label>
-					<ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+					<ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-300 rounded-box w-52">
 						<li>
 							<a href="./#" className="justify-between">
 								Profile
