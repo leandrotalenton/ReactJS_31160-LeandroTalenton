13d2d85ef2e6b734520b5367c72c31d2175a88cd
Leandro T <leandrotalenton@hotmail.com>
2022-04-24 21:56:28 -0300

    se modifica el monto inicial a 1

--- src/App.js
+++ src/App.js
@@ -15,14 +15,14 @@ function App() {
       </NavBar>
       <Hero/>
       <ItemListContainer>
-        <Card name='zapatilla 1' price='21' initial='0' stock='1' />
-        <Card name='zapatilla 2' price='22' initial='0' stock='2' />
-        <Card name='zapatilla 3' price='23' initial='0' stock='3' />
-        <Card name='zapatilla 4' price='24' initial='0' stock='4' />
-        <Card name='zapatilla 5' price='25' initial='0' stock='5' />
-        <Card name='zapatilla 6' price='26' initial='0' stock='6' />
-        <Card name='zapatilla 7' price='27' initial='0' stock='7' />
-        <Card name='zapatilla 8' price='28' initial='0' stock='8' />
+        <Card name='zapatilla 1' price='21' initial='1' stock='1' />
+        <Card name='zapatilla 2' price='22' initial='1' stock='2' />
+        <Card name='zapatilla 3' price='23' initial='1' stock='3' />
+        <Card name='zapatilla 4' price='24' initial='1' stock='4' />
+        <Card name='zapatilla 5' price='25' initial='1' stock='5' />
+        <Card name='zapatilla 6' price='26' initial='1' stock='6' />
+        <Card name='zapatilla 7' price='27' initial='1' stock='7' />
+        <Card name='zapatilla 8' price='28' initial='1' stock='8' />
       </ItemListContainer>
       <Footer/>
     </>
