64a6aecba0fd0b43de903d9b2c1eb0ae4011b91e
Leandro T <leandrotalenton@hotmail.com>
2022-05-04 12:08:18 -0300

    Se realizan las correcciones de la ultima entrega

--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,27 +1,18 @@
-import { useEffect, useState } from "react"
-import { Link, useParams } from "react-router-dom"
-import { itemsData } from "../data/itemsData"
+import { Link } from "react-router-dom"
 
-const ItemDetail = () => {
-    
-    const params = useParams()
-    const [item,setItem] = useState( {} ) 
-
-    useEffect(() => {
-        setItem(itemsData.find(i=>i.id === params.itemId))
-        }, [params.itemId])
+const ItemDetail = (props) => {
     
     return (
     <div className="card lg:card-side bg-base-100 shadow-xl m-4 my-auto">
-        <figure><img src={item.pictureUrl} alt="Album" /></figure>
+        <figure><img src={props.item.pictureUrl} alt="Album" /></figure>
         <div className="card-body">
-            <h2 className="card-title">Item: {item.title}</h2>
-            <p>{item.description}</p>
-            <p>Stock disponible: {item.stock} unidades</p>
-            <p>Precio: ${item.price}</p>
+            <h2 className="card-title">Item: {props.item.title}</h2>
+            <p>{props.item.description}</p>
+            <p>Stock disponible: {props.item.stock} unidades</p>
+            <p>Precio: ${props.item.price}</p>
             <div className="card-actions justify-end">
                 <Link to='/' className="btn btn-primary">Volver</Link>
-                <Link to={`/item/${parseInt(params.itemId) + 1}`} className="btn btn-primary">Siguiente</Link>
+                <Link to={`/item/${parseInt(props.params.itemId) + 1}`} className="btn btn-primary">Siguiente</Link>
             </div>
         </div>
     </div>
--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,10 +1,21 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import ItemDetail from "./ItemDetail";
+import { itemsData } from "../data/itemsData"
+
 
 const ItemDetailContainer = (props) => {
 
+    const params = useParams()
+    const [item,setItem] = useState( {} ) 
+
+    useEffect(() => {
+        setItem(itemsData.find(i=>i.id === params.itemId))
+        }, [params.itemId])
+
         return (
         <div className="mt-[65.94px] min-h-[70vh] flex justify-center content-center  ">
-            <ItemDetail />
+            <ItemDetail item={item} params={params}/>
         </div>
     )
 }
